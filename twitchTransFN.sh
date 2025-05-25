237e5fee75b283111813ab042c6710c17b576d64
q. Thai “RPGReki” Chung <mail@reki.wtf>
2025-05-26 00:44:37 +0200

    Mon May 26 00:44:37 CEST 2025

--- twitchTransFN.sh
+++ twitchTransFN.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-exec ./twitchTransFreeNext.py
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
+
+exec ./twitchTransFN.py
 
